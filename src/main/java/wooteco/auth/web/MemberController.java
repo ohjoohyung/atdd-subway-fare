87b489a4e4c57e118364525d8fc591071b2609c6
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-08-15 00:25:06 +0900

    refactor: * 제거

--- src/main/java/wooteco/auth/web/MemberController.java
+++ src/main/java/wooteco/auth/web/MemberController.java
@@ -1,9 +1,17 @@
 package wooteco.auth.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import wooteco.auth.service.MemberService;
+gimport org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import wooteco.auth.domain.LoginMember;
+import wooteco.auth.service.MemberService;
 import wooteco.auth.web.dto.request.MemberRequest;
 import wooteco.auth.web.dto.response.MemberResponse;
 
